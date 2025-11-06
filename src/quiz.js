748e5745e8e75aade14a2af7dabad14c17656df1
Luana Aguilo <aguilo.luana@gmail.com>
2025-11-06 21:31:42 +0100

    Day 1 complete

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,19 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 2.  getQuestion()
 
-    // 4. shuffleQuestions()
+  // 3. moveToNextQuestion()
 
-    // 5. checkAnswer(answer)
+  // 4. shuffleQuestions()
 
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+
+  // 6. hasEnded()
 }
