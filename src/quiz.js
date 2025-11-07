20ab232e755cc172e3f5dc9095db2b8f83f87319
Luana Aguilo <aguilo.luana@gmail.com>
2025-11-07 16:37:37 +0100

    Day 1 complete

--- src/quiz.js
+++ src/quiz.js
@@ -7,13 +7,27 @@ class Quiz {
     this.currentQuestionIndex = 0;
   }
 
-  // 2.  getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
 
-  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    this.questions.sort(() => Math.random() - 0.5);
+  }
 
-  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    return this.correctAnswers++;
+  }
 
-  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
